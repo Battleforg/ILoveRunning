ef74dffa8f18bedf6f689e6fc52b684e77a9fb75
Yicong Li <lycgaming@163.com>
2019-05-06 15:21:44 -0400

    improve register page

--- angular-iloverunning/src/app/register/register.component.ts
+++ angular-iloverunning/src/app/register/register.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { User } from '../entities/user';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent {
   model = {
     firstName: '',
     lastName: '',
@@ -17,7 +18,23 @@ export class RegisterComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+
+  // Check password
+  checkPassword(): boolean {
+    return (this.model.password1 === this.model.password2);
   }
+
+  // submit new user's information
+  onSubmit(): void {
+    const newUser = new User(
+      this.model.firstName,
+      this.model.lastName,
+      this.model.email,
+      this.model.password1,
+      this.model.zipCode);
+    // TODO: upload this user
+    console.log(newUser);
+  }
+
 
 }
