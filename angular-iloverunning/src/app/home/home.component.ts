e87fe46383f324d0283fc1eb74a0a4449727ae71
Yicong Li <lycgaming@163.com>
2019-05-06 16:04:24 -0400

    improve home page

--- angular-iloverunning/src/app/home/home.component.ts
+++ angular-iloverunning/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserModel } from '../models/userModel';
 
 @Component({
   selector: 'app-home',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  user = new UserModel('Yi', 'Li', 'yili@yl.org', '123', 'kkk');
 
   constructor() { }
 
