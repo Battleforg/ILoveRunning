9a931806cf14fb84f99d6340a2d28d9451b5aa3b
Yicong Li <lycgaming@163.com>
2019-05-07 16:37:17 -0400

    create node-express by express-generator

--- angular-iloverunning/src/app/app-routing.module.ts
+++ angular-iloverunning/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
   {path: '', component: RegisterComponent},
-  {path: 'home', component: HomeComponent}
+  {path: 'home/:uid', component: HomeComponent}
 ];
 
 @NgModule({
