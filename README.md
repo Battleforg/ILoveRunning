2826c0d9a69dd537c6387dad20d43b0fb37e41eb
Yicong Li <lycgaming@163.com>
2019-05-09 17:55:13 -0400

    customer service update

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 Hope this can push me to run
 
 # angular-iloverunning
-To run in development mode: `ng serve`.
+To run in development mode: `ng serve --proxy-config proxy.conf.json`.
+
 
 # node-express-iloverunning
 To run: `npm start`.
--- node-express-iloverunning/services/customer.service.js
+++ node-express-iloverunning/services/customer.service.js
@@ -11,10 +11,40 @@ let customerValidator = new Vadlidator();
 let namePattern = /([A-Za-z\-\'])*/;
 let passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])[a-zA-Z]+$/;
 
+// customer validator schema
+const customerVSchema = {
+  guid: { type: 'string', min: 3 },
+  firstName: { type: 'string', min: 1, max: 50, pattern: namePattern },
+  lastName: { type: 'string', min: 1, max: 50, pattern: namePattern },
+  email: { type: 'string', max: 75 },
+  zipCode: { type: 'string', max: 75 },
+  password: { type: 'string', min: 2, max: 50, pattern: passwordPattern }
+};
+
 // static customer service
 class CustomerService {
   static create(data) {
+    var vres = customerValidator.validate(data, customerVSchema);
 
+    // validation failed
+    if (!(vres === true)) {
+      let errors = {};
+      for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        errors[element.field] = element.message;
+      }
+      throw {
+        name: 'ValidationError',
+        message: errors
+      }
+    }
+
+    let customer = new CustomerModel('c' + counter++, data.firstName,
+      data.lastName, data.email, data.zipCode, data.password);
+    // In memory storage
+    customers[customer.uid] = customer;
+
+    return customer;
   }
 
   static retrieve(uid) {
