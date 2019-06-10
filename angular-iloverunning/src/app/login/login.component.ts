0d5fce13ae3037d7e81c2480e4ba92cff590867e
Yicong Li <lycgaming@163.com>
2019-06-09 21:39:54 -0400

    update node npm typescript angular, etc

--- angular-iloverunning/src/app/login/login.component.ts
+++ angular-iloverunning/src/app/login/login.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  model = {
+    email: '',
+    password: ''
+  };
+  isRemember = false;
+
   constructor() { }
 
   ngOnInit() {
